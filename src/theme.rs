ba18d6ddd7387aba2ce69913242f839a7d5a8d2b
Federico D'Ambrosio <federico.dambrosio@igenius.ai>
2025-11-21 18:21:01 +0100

    feat: Add Solarized Dark/Light, Gruvbox, Tokyo Night, and Catppuccin color themes and update README.

--- src/theme.rs
+++ src/theme.rs
@@ -79,6 +79,98 @@ impl Theme {
                     Color::Rgb(174, 129, 255), // Purple
                 ],
             },
+            "solarized-dark" => Self {
+                background: Color::Rgb(0, 43, 54),
+                text: Color::Rgb(131, 148, 150),
+                title: Color::Rgb(38, 139, 210), // Blue
+                border: Color::Rgb(88, 110, 117),
+                border_selected: Color::Rgb(181, 137, 0), // Yellow
+                legend_text: Color::Rgb(131, 148, 150),
+                legend_dim: Color::Rgb(88, 110, 117),
+                palette: vec![
+                    Color::Rgb(181, 137, 0),   // Yellow
+                    Color::Rgb(203, 75, 22),   // Orange
+                    Color::Rgb(220, 50, 47),   // Red
+                    Color::Rgb(211, 54, 130),  // Magenta
+                    Color::Rgb(108, 113, 196), // Violet
+                    Color::Rgb(38, 139, 210),  // Blue
+                    Color::Rgb(42, 161, 152),  // Cyan
+                    Color::Rgb(133, 153, 0),   // Green
+                ],
+            },
+            "solarized-light" => Self {
+                background: Color::Rgb(253, 246, 227),
+                text: Color::Rgb(101, 123, 131),
+                title: Color::Rgb(38, 139, 210), // Blue
+                border: Color::Rgb(147, 161, 161),
+                border_selected: Color::Rgb(181, 137, 0), // Yellow
+                legend_text: Color::Rgb(101, 123, 131),
+                legend_dim: Color::Rgb(147, 161, 161),
+                palette: vec![
+                    Color::Rgb(181, 137, 0),   // Yellow
+                    Color::Rgb(203, 75, 22),   // Orange
+                    Color::Rgb(220, 50, 47),   // Red
+                    Color::Rgb(211, 54, 130),  // Magenta
+                    Color::Rgb(108, 113, 196), // Violet
+                    Color::Rgb(38, 139, 210),  // Blue
+                    Color::Rgb(42, 161, 152),  // Cyan
+                    Color::Rgb(133, 153, 0),   // Green
+                ],
+            },
+            "gruvbox" => Self {
+                background: Color::Rgb(40, 40, 40),
+                text: Color::Rgb(235, 219, 178),
+                title: Color::Rgb(215, 153, 33), // Yellow
+                border: Color::Rgb(146, 131, 116),
+                border_selected: Color::Rgb(254, 128, 25), // Orange
+                legend_text: Color::Rgb(235, 219, 178),
+                legend_dim: Color::Rgb(146, 131, 116),
+                palette: vec![
+                    Color::Rgb(204, 36, 29),   // Red
+                    Color::Rgb(152, 151, 26),  // Green
+                    Color::Rgb(215, 153, 33),  // Yellow
+                    Color::Rgb(69, 133, 136),  // Blue
+                    Color::Rgb(177, 98, 134),  // Purple
+                    Color::Rgb(104, 157, 106), // Aqua
+                    Color::Rgb(254, 128, 25),  // Orange
+                ],
+            },
+            "tokyo-night" => Self {
+                background: Color::Rgb(26, 27, 38),
+                text: Color::Rgb(169, 177, 214),
+                title: Color::Rgb(122, 162, 247), // Blue
+                border: Color::Rgb(86, 95, 137),
+                border_selected: Color::Rgb(255, 158, 100), // Orange
+                legend_text: Color::Rgb(169, 177, 214),
+                legend_dim: Color::Rgb(86, 95, 137),
+                palette: vec![
+                    Color::Rgb(247, 118, 142), // Red
+                    Color::Rgb(158, 206, 106), // Green
+                    Color::Rgb(224, 175, 104), // Yellow
+                    Color::Rgb(122, 162, 247), // Blue
+                    Color::Rgb(187, 154, 247), // Magenta
+                    Color::Rgb(125, 207, 255), // Cyan
+                    Color::Rgb(255, 158, 100), // Orange
+                ],
+            },
+            "catppuccin" => Self {
+                background: Color::Rgb(30, 30, 46),
+                text: Color::Rgb(205, 214, 244),
+                title: Color::Rgb(137, 180, 250), // Blue
+                border: Color::Rgb(88, 91, 112),
+                border_selected: Color::Rgb(249, 226, 175), // Yellow
+                legend_text: Color::Rgb(205, 214, 244),
+                legend_dim: Color::Rgb(88, 91, 112),
+                palette: vec![
+                    Color::Rgb(243, 139, 168), // Red
+                    Color::Rgb(166, 227, 161), // Green
+                    Color::Rgb(249, 226, 175), // Yellow
+                    Color::Rgb(137, 180, 250), // Blue
+                    Color::Rgb(203, 166, 247), // Mauve
+                    Color::Rgb(148, 226, 213), // Teal
+                    Color::Rgb(250, 179, 135), // Peach
+                ],
+            },
             _ => Self::default(),
         }
     }
